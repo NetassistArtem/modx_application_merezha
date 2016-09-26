2bc86bc787eb4f6c07eac3350b981d78aecc85dd
Artem <k.artem@netassist.ua>
2016-09-26 16:26:09 +0300

    Simple MODX application

---
+++ core/config/config.inc.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *  MODX Configuration file
+ */
+$database_type = 'mysql';
+$database_server = 'localhost';
+$database_user = 'user_dev1';
+$database_password = 'L2YRsJzYVC89ATBE';
+$database_connection_charset = 'utf8';
+$dbase = 'devsite1';
+$table_prefix = 'modx_';
+$database_dsn = 'mysql:host=localhost;dbname=devsite1;charset=utf8';
+$config_options = array (
+);
+$driver_options = array (
+);
+
+$lastInstallTime = 1473590113;
+
+$site_id = 'modx57d533615b0935.84854410';
+$site_sessionname = 'SN57d531fd96c3e';
+$https_port = '443';
+$uuid = '60274af1-bee2-4b3e-97e4-dae077b0cacd';
+
+if (!defined('MODX_CORE_PATH')) {
+    $modx_core_path= '/home/artem/public_html/test04/core/';
+    define('MODX_CORE_PATH', $modx_core_path);
+}
+if (!defined('MODX_PROCESSORS_PATH')) {
+    $modx_processors_path= '/home/artem/public_html/test04/core/model/modx/processors/';
+    define('MODX_PROCESSORS_PATH', $modx_processors_path);
+}
+if (!defined('MODX_CONNECTORS_PATH')) {
+    $modx_connectors_path= '/home/artem/public_html/test04/connectors/';
+    $modx_connectors_url= '/connectors/';
+    define('MODX_CONNECTORS_PATH', $modx_connectors_path);
+    define('MODX_CONNECTORS_URL', $modx_connectors_url);
+}
+if (!defined('MODX_MANAGER_PATH')) {
+    $modx_manager_path= '/home/artem/public_html/test04/manager/';
+    $modx_manager_url= '/manager/';
+    define('MODX_MANAGER_PATH', $modx_manager_path);
+    define('MODX_MANAGER_URL', $modx_manager_url);
+}
+if (!defined('MODX_BASE_PATH')) {
+    $modx_base_path= '/home/artem/public_html/test04/';
+    $modx_base_url= '/';
+    define('MODX_BASE_PATH', $modx_base_path);
+    define('MODX_BASE_URL', $modx_base_url);
+}
+if(defined('PHP_SAPI') && (PHP_SAPI == "cli" || PHP_SAPI == "embed")) {
+    $isSecureRequest = false;
+} else {
+    $isSecureRequest = ((isset ($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on') || $_SERVER['SERVER_PORT'] == $https_port);
+}
+if (!defined('MODX_URL_SCHEME')) {
+    $url_scheme=  $isSecureRequest ? 'https://' : 'http://';
+    define('MODX_URL_SCHEME', $url_scheme);
+}
+if (!defined('MODX_HTTP_HOST')) {
+    if(defined('PHP_SAPI') && (PHP_SAPI == "cli" || PHP_SAPI == "embed")) {
+        $http_host='devsite1.ua';
+        define('MODX_HTTP_HOST', $http_host);
+    } else {
+        $http_host= array_key_exists('HTTP_HOST', $_SERVER) ? $_SERVER['HTTP_HOST'] : 'devsite1.ua';
+        if ($_SERVER['SERVER_PORT'] != 80) {
+            $http_host= str_replace(':' . $_SERVER['SERVER_PORT'], '', $http_host); // remove port from HTTP_HOST
+        }
+        $http_host .= ($_SERVER['SERVER_PORT'] == 80 || $isSecureRequest) ? '' : ':' . $_SERVER['SERVER_PORT'];
+        define('MODX_HTTP_HOST', $http_host);
+    }
+}
+if (!defined('MODX_SITE_URL')) {
+    $site_url= $url_scheme . $http_host . MODX_BASE_URL;
+    define('MODX_SITE_URL', $site_url);
+}
+if (!defined('MODX_ASSETS_PATH')) {
+    $modx_assets_path= '/home/artem/public_html/test04/assets/';
+    $modx_assets_url= '/assets/';
+    define('MODX_ASSETS_PATH', $modx_assets_path);
+    define('MODX_ASSETS_URL', $modx_assets_url);
+}
+if (!defined('MODX_LOG_LEVEL_FATAL')) {
+    define('MODX_LOG_LEVEL_FATAL', 0);
+    define('MODX_LOG_LEVEL_ERROR', 1);
+    define('MODX_LOG_LEVEL_WARN', 2);
+    define('MODX_LOG_LEVEL_INFO', 3);
+    define('MODX_LOG_LEVEL_DEBUG', 4);
+}
+if (!defined('MODX_CACHE_DISABLED')) {
+    $modx_cache_disabled= false;
+    define('MODX_CACHE_DISABLED', $modx_cache_disabled);
+}
---
+++ manager/config.core.php
@@ -0,0 +1,4 @@
+<?php
+define('MODX_CORE_PATH', '/home/artem/public_html/test04/core/');
+define('MODX_CONFIG_KEY', 'config');
+?>
